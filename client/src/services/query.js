33ed80aa3dda0c477489ece7910667381b03437b
Brian Park <park-brian@users.noreply.github.com>
2020-06-17 16:04:21 -0400

    do not process query body

--- client/src/services/query.js
+++ client/src/services/query.js
@@ -2,20 +2,10 @@
 
 export async function postJSON(url, data) {
 
-    // create shallow copy of data
-    let body = {};
-    for (let key in data) {
-        let value = data[key];
-
-        //  add the key to the object, coercing numeric strings to numbers
-        if (value.length && value !== null && value !== undefined)
-            body[key] = isNaN(value) ? value : +value;
-    }
-
     const response = await fetch(url, {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(body)
+        body: JSON.stringify(data)
     });
 
     if (response.ok) {
