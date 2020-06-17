211ee5b70a43321b00e675619d964c7562b85e49
Brian Park <park-brian@users.noreply.github.com>
2020-06-17 15:36:24 -0400

    add R dependencies

---
+++ install.R
@@ -0,0 +1,28 @@
+install.packages(
+    c(
+        'abind',
+        'deldir',
+        'doParallel',
+        'dotCall64',
+        'fields',
+        'foreach',
+        'goftest',
+        'iterators',
+        'jsonlite',
+        'maps',
+        'misc3d',
+        'polyclip',
+        'raster',
+        'Rcpp',
+        'sp',
+        'spam',
+        'sparr',
+        'sparrpowR',
+        'spatstat',
+        'spatstat.data',
+        'spatstat.utils',
+        'tensor'
+    ), 
+    lib = .Library, 
+    repos='https://cloud.r-project.org/'
+)
