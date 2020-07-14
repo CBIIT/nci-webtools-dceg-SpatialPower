fe28823ed0b681974e5253f47d0997458f5dfba3
Brian Park <park-brian@users.noreply.github.com>
2020-07-14 01:42:36 -0400

    store state in redux store

--- client/src/pages/calculate/calculate.js
+++ client/src/pages/calculate/calculate.js
@@ -1,57 +1,105 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import Alert from 'react-bootstrap/Alert';
+import { useSelector, useDispatch } from 'react-redux';
+import { LoadingOverlay } from '@cbiitss/react-components';
+import { actions as resultsActions } from '../../services/store/results';
+import { actions as messagesActions } from '../../services/store/messages';
 import { InputForm } from './input-form';
 import { Results } from './results';
-import { postJSON } from '../../services/query';
+import { fetchJSON, postJSON } from '../../services/query';
+const { merge: _mergeResults, reset: _resetResults } = resultsActions;
+const { merge: _mergeMessages, reset: _resetMessages, removeByIndex: _removeMessageByIndex } = messagesActions;
 
-export function Calculate() {
-    const [params, setParams] = useState({});
-    const [results, setResults] = useState({});
-    const [messages, setMessages] = useState([]);
+export function Calculate({match}) {
+    const dispatch = useDispatch();
+    const results = useSelector(state => state.results);
+    const messages = useSelector(state => state.messages);
+    const mergeResults = results => dispatch(_mergeResults(results));
+    const mergeMessages = messages => dispatch(_mergeMessages(messages));
+    const resetResults = _ => dispatch(_resetResults());
+    const resetMessages = _ => dispatch(_resetMessages());
+    const removeMessageByIndex = index => dispatch(_removeMessageByIndex(index));
+
+    // run once
+    useEffect(() => {
+        const { id } = match.params;
+        if (id) loadResults(id);
+    }, []);
 
     async function handleSubmit(params) {
-        setParams(params);
-        setResults({});
-        setMessages([]);
+        console.log(params);
+
+        resetResults();
+        resetMessages();
 
         try {
-            console.log(params);
+            mergeResults({loading: true});
             const results = await postJSON('submit', params);
 
             if (params.queue) {
                 // if the request was enqueued, notify the user
-                setMessages([{type: 'primary', text: `Your request has been enqueued, and results will be sent to: ${params.email}.`}]);
+                mergeMessages([{type: 'primary', text: `Your request has been enqueued. Results will be sent to: ${params.email}.`}]);
             } else {
                 // otherwise, show results
-                setResults(results);
+                mergeResults(results);
             }
 
         } catch (error) {
-            setMessages([{type: 'danger', text: error}]);
+            mergeMessages([{type: 'danger', text: error}]);
+        } finally {
+            mergeResults({loading: false});
         }
     }
 
-    function removeMessage(index) {
-        setMessages(messages.filter((e, i) => i !== index))
-    }    
+    function handleReset() {
+        // do not call resetParams, as it initiates handleReset
+        resetResults();
+        resetMessages();
+    }
 
-    return <div className="container my-4">
+    async function loadResults(id) {
+        resetResults();
+        resetMessages();
+
+        try {
+            mergeResults({loading: true});
+            const results = await fetchJSON(`fetch-results/?id=${id}`);
+            if (results) {
+                mergeResults(results);
+            } else {
+                mergeMessages([{type: 'danger', text: `No results could be found for the specified id.`}]);
+            }
+        } catch (error) {
+            mergeMessages([{type: 'danger', text: error}]);
+        } finally {
+            mergeResults({loading: false});
+        }
+    }
+
+    return <div className="container py-4">
+        <LoadingOverlay active={results.loading} />
         <h1 className="h4 mb-4">Spatial Risk Calculation</h1>
         <div className="row">
             <div className="col-md-4">
                 <div className="card shadow-sm h-100">
-                    <InputForm className="card-body" onSubmit={handleSubmit}/>
+                    <InputForm className="card-body" onSubmit={handleSubmit} onReset={handleReset}/>
                 </div>
             </div>
             <div className="col-md-8">
                 <div className="card shadow-sm h-100">
                     <div className="card-body">
-                        {messages.map((message, i) => <Alert variant={message.type} dismissible onClose={e => removeMessage(i)}>{message.text}</Alert>)}
+                        {messages.map((message, i) => 
+                            <Alert 
+                                key={`results-alert-${i}`} 
+                                variant={message.type} 
+                                dismissible 
+                                onClose={e => removeMessageByIndex(i)}>
+                                {message.text}
+                            </Alert>)}
                         {<Results results={results} />}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 }
--- client/src/pages/calculate/input-form.js
+++ client/src/pages/calculate/input-form.js
@@ -1,23 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { actions as paramsActions, getInitialState as getInitialParams } from '../../services/store/params';
+const { merge: _mergeParams, reset: _resetParams } = paramsActions;
 
-
-export function InputForm({ className = '', onSubmit }) {
-    const [params, setParams] = useState({
-        simTotal: '',
-        xCases: '',
-        yCases: '',
-        nCase: '',
-        nControl: '',
-        queue: true,
-        email: '',
-    });
+export function InputForm({ 
+    className = '', 
+    onSubmit = e => { }, 
+    onReset = e => { } 
+}) {
+    const dispatch = useDispatch();
+    const params = useSelector(state => state.params);
+    const mergeParams = value => dispatch(_mergeParams(value));
+    const resetParams = _ => dispatch(_resetParams());
 
     function handleChange(event) {
         const target = event.target;
         const name = target.name;
-        let value = target.value;
+        let value = '';
 
-        switch(target.type) {
+        switch (target.type) {
             case 'checkbox':
                 value = Boolean(target.checked);
                 break;
@@ -28,7 +29,9 @@ export function InputForm({ className = '', onSubmit }) {
                 value = target.value;
         }
 
-        setParams({ ...params, [name]: value });
+        mergeParams({
+            [name]: value
+        });
     }
 
     function handleSubmit(event) {
@@ -38,64 +41,82 @@ export function InputForm({ className = '', onSubmit }) {
         }
     }
 
+    function handleReset(event) {
+        event.preventDefault();
+        resetParams();
+        if (onReset) {
+            onReset(getInitialParams());
+        }
+    }
+
     return <form className={className}>
-            <div className="form-group">
-                <label htmlFor="simTotal" className="font-weight-semibold">simTotal</label>
-                <input type="number" id="simTotal" name="simTotal" className="form-control" value={params.simTotal} onChange={handleChange} />
-            </div>
+        <div className="form-group">
+            <label htmlFor="simTotal" className="font-weight-semibold">simTotal</label>
+            <input type="number" id="simTotal" name="simTotal" className="form-control" value={params.simTotal} onChange={handleChange} />
+        </div>
 
-            <div className="form-group">
-                <label htmlFor="xCases" className="font-weight-semibold">xCases</label>
-                <input type="number" id="xCases" name="xCases" className="form-control" value={params.xCases} onChange={handleChange} />
-            </div>
+        <div className="form-group">
+            <label htmlFor="xCases" className="font-weight-semibold">xCases</label>
+            <input type="number" id="xCases" name="xCases" className="form-control" value={params.xCases} onChange={handleChange} />
+        </div>
 
-            <div className="form-group">
-                <label htmlFor="yCases" className="font-weight-semibold">yCases</label>
-                <input type="number" id="yCases" name="yCases" className="form-control" value={params.yCases} onChange={handleChange} />
-            </div>
+        <div className="form-group">
+            <label htmlFor="yCases" className="font-weight-semibold">yCases</label>
+            <input type="number" id="yCases" name="yCases" className="form-control" value={params.yCases} onChange={handleChange} />
+        </div>
 
-            <div className="form-group">
-                <label htmlFor="nCase" className="font-weight-semibold">nCase</label>
-                <input type="number" id="nCase" name="nCase" className="form-control" value={params.nCase} onChange={handleChange} />
-            </div>
+        <div className="form-group">
+            <label htmlFor="nCase" className="font-weight-semibold">nCase</label>
+            <input type="number" id="nCase" name="nCase" className="form-control" value={params.nCase} onChange={handleChange} />
+        </div>
 
-            <div className="form-group">
-                <label htmlFor="nControl" className="font-weight-semibold">nControl</label>
-                <input type="number" id="nControl" name="nControl" className="form-control" value={params.nControl} onChange={handleChange} />
-            </div>
+        <div className="form-group">
+            <label htmlFor="nControl" className="font-weight-semibold">nControl</label>
+            <input type="number" id="nControl" name="nControl" className="form-control" value={params.nControl} onChange={handleChange} />
+        </div>
 
-            <div className="form-group custom-control custom-checkbox">
-                <input type="checkbox" className="custom-control-input" id="submit-queue" name="queue" checked={params.queue} onChange={handleChange} />
-                <label className="custom-control-label" htmlFor="submit-queue">Submit this job to a queue</label>
-            </div>
+        <div className="form-group custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="submit-queue" name="queue" checked={params.queue} onChange={handleChange} />
+            <label className="custom-control-label" htmlFor="submit-queue">Submit this job to a queue</label>
+        </div>
 
-            <div className="form-group">
-                <label
-                    htmlFor="email"
-                    className="font-weight-semibold">
-                    Email address
+        <div className="form-group">
+            <label
+                htmlFor="email"
+                className="font-weight-semibold">
+                Email address
             </label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    className="form-control"
-                    aria-describedby="email-help"
-                    value={params.email}
-                    disabled={!params.queue}
-                    onChange={handleChange} />
-                <small
-                    id="email-help"
-                    className="form-text text-muted">
-                    Results will be sent to the specified email.
+            <input
+                type="email"
+                id="email"
+                name="email"
+                className="form-control"
+                aria-describedby="email-help"
+                value={params.email}
+                disabled={!params.queue}
+                onChange={handleChange} />
+            <small
+                id="email-help"
+                className="form-text text-muted">
+                Results will be sent to the specified email.
             </small>
-            </div>
+        </div>
+
+        <div className="text-right">
+            <button
+                type="reset"
+                className="btn btn-outline-danger mx-1"
+                onClick={handleReset}>
+                Reset
+            </button>
 
             <button
                 type="submit"
                 className="btn btn-primary"
                 onClick={handleSubmit}>
                 Submit
-        </button>
+            </button>
+        </div>
+
     </form>
 }
