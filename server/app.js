56cb7cd20c0cdf0906b0c4951f84f41548e5c071
Brian Park <park-brian@users.noreply.github.com>
2020-07-14 01:43:50 -0400

    fetch results from s3

--- server/app.js
+++ server/app.js
@@ -3,38 +3,24 @@ const path = require('path');
 const crypto = require('crypto');
 const express = require('express');
 const compression = require('compression')
-const { Producer } = require('sqs-producer');
+const AWS = require('aws-sdk');
 const r = require('r-wrapper');
 const config = require('./config.json');
 const logger = require('./utils/logger');
 
-// create sqs producer
-const producer = Producer.create({
-    queueUrl: config.queue.url,
-    region: config.queue.region,
-    accessKeyId: config.queue.accessKeyId,
-    secretAccessKey: config.queue.secretAccessKey,
-});
-
-// create express app
 const app = express();
 
 // serve public folder
-app.use(express.static('public'));
+app.use(express.static(config.server.static));
 
 // serve results folder
-app.use('/results', express.static('results'));
+app.use('/results', express.static(config.results.folder));
 
-// json parser middleware
+// parse json requests
 app.use(express.json());
 
 // compress all responses
 app.use(compression());
-
-// create folders
-for (let folder of [config.logging.folder, config.results.folder])
-    if (!fs.existsSync(folder))
-        fs.mkdirSync(folder, {recursive: true})
 
 // handle calculation submission
 app.post('/submit', async (request, response) => {
@@ -42,20 +28,22 @@ app.post('/submit', async (request, response) => {
         const id = crypto.randomBytes(16).toString('hex');
         if (request.body.queue) {
             // enqueue message and send a response with the request id
-            const messageBody = {id, ...request.body};
-            await producer.send([{id, body: JSON.stringify(messageBody)}]);
-            response.json({id}); // return message id
+            await new AWS.SQS().sendMessage({
+                QueueUrl: config.queue.url,
+                MessageDeduplicationId: id,
+                MessageGroupId: id,
+                MessageBody: JSON.stringify({id, ...request.body})
+            }).promise();
+            response.json({id});
         } else {
             // otherwise, perform calculation and return results
-            response.json(r(
-                path.resolve(__dirname, 'calculate.R'), // path to R source file
-                'calculate', // name of method to call
-                [{
-                    ...request.body, 
-                    workingDirectory: path.resolve(config.results.folder),
-                    id, 
-                }]
-            ));
+            const workingDirectory = path.resolve(config.results.folder, id);
+            const params = {...request.body, id, workingDirectory};
+            const sourcePath = path.resolve(__dirname, 'calculate.R');
+
+            await fs.promises.mkdir(workingDirectory, {recursive: true});
+            const results = r(sourcePath, 'calculate', [params]);
+            response.json(results);
         }
     } catch(error) {
         logger.error(error);
@@ -67,43 +55,40 @@ app.post('/submit', async (request, response) => {
 // the application from a queue-generated url
 app.get('/fetch-results', async (request, response) => {
     try {
-        // find all objects with the specified prefix
-        const id = request.params.id; 
+        const s3 = new AWS.S3();
+        const { id } = request.query; 
 
         // validate id format
-        if (!/^[a-z0-9]$/i.test(id))
-            response.json(false);
+        if (!/^[a-z0-9]+$/i.test(id)) {
+            console.log(id);
+            return response.json(false);
+        }
 
-        const s3 = new AWS.S3({
-            apiVersion: '2006-03-01',
-            accessKeyId: config.s3.accessKeyId,
-            secretAccessKey: config.s3.secretAccessKey,
-            params: {Bucket: config.s3.bucket}
-        });
-    
+        // ensure output directory exists
+        const resultsFolder = path.resolve(config.results.folder, id);
+        await fs.promises.mkdir(resultsFolder, {recursive: true});
+
         // find objects which use the specified id as the prefix
-        const objects = await s3.listObjectsV2({
-            prefix: id
+        const objects =  await s3.listObjectsV2({
+            Bucket: config.s3.bucket,
+            Prefix: `${config.s3.outputKeyPrefix}${id}/`,
         }).promise();
 
         // download results
-        for (let key of objects.Contents) {
-
-            // only download png/json files which include the specified id
-            if (!(/\.(png|json)$/.test(key) && key.includes(id))) {
-                continue;
-            }
-
-            logger.info(`Downloading result: ${key}`);
-            const object = await s3.getObject(key).promise();
-
-            fs.writeFileSync(
-                path.resolve(config.folder.result, key),
+        for (let {Key} of objects.Contents) {
+            logger.info(`Downloading result: ${Key}`);
+            const object = await s3.getObject({
+                Bucket: config.s3.bucket,
+                Key
+            }).promise();
+            const filename = path.basename(Key);
+            await fs.promises.writeFile(
+                path.resolve(resultsFolder, filename),
                 object.Body
             );
         }
 
-        let resultsFile = path.resolve(config.results.folder, `${id}.json`);
+        let resultsFile = path.resolve(resultsFolder, `results.json`);
         if (fs.existsSync(resultsFile))
             response.sendFile(resultsFile);
         else
@@ -115,7 +100,16 @@ app.get('/fetch-results', async (request, response) => {
     }
 });
 
+app.listen(config.server.port, () => {
+    // update aws configuration if supplied
+    if (config.aws) {
+        AWS.config.update(config.aws);
+    }
 
-app.listen(config.port, () => {
-    logger.info(`Application is running on port: ${config.port}`)
+    // create required folders 
+    for (let folder of [config.logs.folder, config.results.folder]) {
+        fs.mkdirSync(folder, {recursive: true});
+    }
+
+    logger.info(`Application is running on port: ${config.server.port}`)
 });
