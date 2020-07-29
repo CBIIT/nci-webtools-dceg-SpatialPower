b181ab38e15eaabe0f5dda8d0517e9b42a4ee0fa
Brian Park <park-brian@users.noreply.github.com>
2020-07-29 01:20:23 -0400

    update actions

--- client/src/services/store/params.js
+++ client/src/services/store/params.js
@@ -2,12 +2,28 @@ import { createSlice } from '@reduxjs/toolkit';
 import { mergeObject } from './utils';
 
 export const getInitialState = () => ({
-    simTotal: '',
-    xCases: '',
-    yCases: '',
-    nCase: '',
-    nControl: '',
-    queue: true,
+    win: 'unit.circle',
+    sim_total: '',
+    x_case: '',
+    y_case: '',
+    samp_case: '',
+    samp_control: '',
+    x_control: '',
+    y_control: '',
+    n_case: '',
+    n_control: '',
+    npc_control: '',
+    r_case: '',
+    r_control: '',
+    s_case: '',
+    s_control: '',
+    l_case: '',
+    l_control: '',
+    e_control: '',
+    lower_tail: 0.025,
+    upper_tail: 0.975,
+    cascon: false,
+    queue: false,
     email: '',
 });
 
@@ -15,7 +31,7 @@ export const { actions, reducer } = createSlice({
     name: 'params',
     initialState: getInitialState(),
     reducers: {
-        merge: mergeObject,
-        reset: getInitialState,
+        mergeParams: mergeObject,
+        resetParams: getInitialState,
     }
 });
