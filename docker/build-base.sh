58ee9fd3f4f55b19bab1e1b5f4fffd9e3fef407b
Brian Park <park-brian@users.noreply.github.com>
2020-08-19 16:16:45 -0400

    remove unused scripts

--- docker/build-base.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build -t cbiitss/spatial-power:base -f base.dockerfile .
--- docker/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker rm -f spatial-power spatial-power-processor
-docker build -t cbiitss/spatial-power -f app.dockerfile ..
-docker run -d -p 80:8000 --name spatial-power --restart always cbiitss/spatial-power
-docker run -d --name spatial-power-processor --restart always cbiitss/spatial-power npm run start-queue-worker
