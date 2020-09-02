238b9c737041f42e2d878a583094a47cc30deddb
bchen49 <benchen4998@gmail.com>
2020-09-02 13:33:34 -0400

    increase input panel width

--- client/src/pages/calculate/calculate.js
+++ client/src/pages/calculate/calculate.js
@@ -129,14 +129,14 @@ export function Calculate({ match }) {
         <h1 className="sr-only">SparrpowR</h1>
         <LoadingOverlay active={results.loading} />
         <div className="row">
-            <div className="col-xl-3 col-lg-4 col-md-5 mb-3">
+            <div className="col-xl-4 col-lg-4 col-md-5 mb-3">
                 <Card className="shadow-sm h-100">
                     <Card.Body>
                         <InputForm onSubmit={handleSubmit} onReset={handleReset} />
                     </Card.Body>
                 </Card>
             </div>
-            <div className="col-xl-9 col-lg-8 col-md-7 mb-3">
+            <div className="col-xl-8 col-lg-8 col-md-7 mb-3">
                 {messages.map((message, i) =>
                     <Alert
                         className="white-space-pre-wrap"
