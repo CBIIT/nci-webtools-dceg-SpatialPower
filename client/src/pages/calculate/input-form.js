f275d901a61033de895a2b9dfca0fdad328b3e28
bchen49 <benchen4998@gmail.com>
2020-09-22 15:05:04 -0400

    update default values for rectangles

--- client/src/pages/calculate/input-form.js
+++ client/src/pages/calculate/input-form.js
@@ -84,13 +84,13 @@ export function InputForm({
                 newParams.height = 2
             }
 
-            newParams.x_case = [newParams.width / 2]
-            newParams.y_case = [newParams.height / 2]
+            newParams.x_case = Number(newParams.x_origin) + Number([newParams.width / 2])
+            newParams.y_case = Number(newParams.y_origin) + Number([newParams.height / 2])
             newParams.r_case = [Math.floor(Math.min(newParams.width / 2, newParams.height / 2) * 10) / 10]
             newParams.s_case = [Math.floor(Math.min(newParams.width / 3, newParams.height / 3) * 10) / 10]
 
-            newParams.x_control = [newParams.width / 2]
-            newParams.y_control = [newParams.height / 2]
+            newParams.x_control = Number(newParams.x_origin) + Number([newParams.width / 2])
+            newParams.y_control = Number(newParams.y_origin) + Number([newParams.height / 2])
             newParams.s_control = [Math.floor(Math.min(newParams.width / 3, newParams.height / 3) * 10) / 10]
         }
 
