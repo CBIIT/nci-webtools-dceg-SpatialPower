e95052705ac6ed0d2fc4e0cc957eb97b37d55776
bchen49 <benchen4998@gmail.com>
2020-10-02 15:29:01 -0400

    fix runtime

--- server/queue-worker.js
+++ server/queue-worker.js
@@ -69,7 +69,7 @@ async function processMessage(params) {
         const results = await r(sourcePath, 'calculate', [{...params, directory}]);
         const end = new Date().getTime();
 
-        const time = start - end;
+        const time = end - start;
         const minutes = Math.floor(time / 60000);
         var seconds = ((time % 60000) / 1000).toFixed(0);
         
