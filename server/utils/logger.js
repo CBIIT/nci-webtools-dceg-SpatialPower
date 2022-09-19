c5d263449afa1b2633702708029a61c69e8718f0
Brian Park <park-brian@users.noreply.github.com>
2022-09-19 16:54:30 -0400

    add more descriptive errors in logs

--- server/utils/logger.js
+++ server/utils/logger.js
@@ -1,46 +1,51 @@
-// const winston = require('winston');
-// winston.emitErrs = true;
-const path = require('path');
-const { createLogger, format, transports } = require('winston');
-const { folder: logFolder, level: logLevel } = require('../config.json').logs;
-require('winston-daily-rotate-file');
+const path = require("path");
+const util = require("util");
+const fs = require("fs");
+const { createLogger, format, transports, info } = require("winston");
+const logConfig = require("../config.json").logs;
+require("winston-daily-rotate-file");
 
-module.exports = new createLogger({
-  level: logLevel || 'info',
-  format: format.combine(
-    format.errors({ stack: true }), // <-- use errors format
-    // format.colorize(),
-    format.timestamp(),
-    format.prettyPrint(),
-    format.label({ label: '[SPARRPOWR-SERVER]' }),
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
-    }),
-    format.printf(info => `[${info.timestamp}] [${info.level}] ${
-        info.level === 'error' ? info.stack : info.message
-    }`),
-    //
-    // The simple format outputs
-    // `${level}: ${message} ${[Object with everything else]}`
-    //
-    // format.simple(),
-    //
-    // Alternatively you could use this custom printf format if you
-    // want to control where the timestamp comes in your final message.
-    // Try replacing `format.simple()` above with this:
-    //
-  ),
-  transports: [
-    new (transports.DailyRotateFile)({
-      filename: path.resolve(logFolder, 'application-%DATE%.log'),
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: false,
-      maxSize: '1024m',
-      timestamp: true,
-      maxFiles: '1d',
-      prepend: true
-    }),
-    new transports.Console()
-  ],
-  exitOnError: false
-});
+function objectToString(obj) {
+  return ["string", "number"].includes(typeof obj)
+    ? obj
+    : util.inspect(obj, { depth: null, compact: true, breakLength: Infinity });
+}
+
+function formatLogMessage({ label, timestamp, level, message }) {
+  return [
+    [label, process.pid, timestamp, level]
+      .filter(Boolean)
+      .map((s) => `[${s}]`)
+      .join(" "),
+    objectToString(message),
+  ].join(" - ");
+}
+
+function getLogger(name, config = logConfig) {
+  const { folder, level } = config;
+  fs.mkdirSync(folder, { recursive: true });
+
+  return new createLogger({
+    level: level || "info",
+    format: format.combine(
+      format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      format.label({ label: name }),
+      format.printf(formatLogMessage)
+    ),
+    transports: [
+      new transports.Console(),
+      new transports.DailyRotateFile({
+        filename: path.resolve(folder, `${name}-%DATE%.log`),
+        datePattern: "YYYY-MM-DD-HH",
+        zippedArchive: false,
+        maxSize: "1024m",
+        timestamp: true,
+        maxFiles: "1d",
+        prepend: true,
+      }),
+    ],
+    exitOnError: false,
+  });
+}
+
+module.exports = getLogger;
